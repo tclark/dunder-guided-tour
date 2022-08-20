25982207af542d23a839d19babb4effd85eb259f
Tom Clark <tom@runawayplay.com>
2022-08-20 13:31:43 +1200

    added solutions for redirector

--- contexts/redirector.py
+++ contexts/redirector.py
@@ -9,9 +9,22 @@ class Redirector:
 
     def __init__(self, outfilename):
         self.outfilename = outfilename
+        self.outfile = None
+        self.saved_stdout = None
 
     def __enter__(self):
-        pass
+        self.outfile = open(self.outfilename, 'w')
+        self.saved_stdout = sys.stdout
+        sys.stdout = self.outfile
 
     def __exit__(self, ex_type, ex_val, ex_trace):
-        pass
+        if self.outfile:
+            self.outfile.close()
+        sys.stdout = self.saved_stdout
+
+
+if __name__ == '__main__':
+    with Redirector('captured'):
+        print('I am Arthur, King of the Britons!')
+        print('King of the who?')
+    print("Well, I didn't vote for you")    
