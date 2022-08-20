d40dc506088a9d9168bcabbf3302cff7a71a624f
Tom Clark <tom@runawayplay.com>
2022-08-20 13:19:50 +1200

    added solutions for cheese_shop

--- iterators/cheese_shop.py
+++ iterators/cheese_shop.py
@@ -21,11 +21,53 @@ class CheeseShop:
         # inventory is a list of Cheeses
         self.inventory = inventory
 
+    def __iter__(self):
+        for cheese in self.inventory:
+            # It makes me happy to write a line like the one below.
+            yield cheese
 
 # Now implement a custom iterator class for CheeseShops
 # by implementing __next__(). Modify your CheeseShop to
 # use this CheeseShopIterator
 
-class CheeseShopIterator:
-    pass
+class CheeseShop2:
+    def __init__(self, proprietor, inventory):
+        self.proprietor = proprietor
+        # inventory is a list of Cheeses
+        self.inventory = inventory
 
+    def __iter__(self):
+        return CheeseShopIterator(self)
+
+class CheeseShopIterator:
+    def __init__(self, shop):
+        self.inventory = shop.inventory
+        self.index = 0
+
+    def __next__(self):
+        if self.index >= len(self.shop):
+            raise StopIteration()
+        item = self.inventory[self.index]
+        self.index += 1
+        return item
+
+    def __iter__(self):
+        # An iterator should return itself
+        return self
+
+if __name__ == '__main__':
+    cheeses = [
+          Cheese('Brie', 4)
+        , Cheese('Edam', 1)
+        , Cheese('Parmesan', 0)
+    ]
+
+shop1 = CheeseShop('Wensleydale', cheeses)
+print('Generator-based iterator')
+for cheese in shop1:
+    print(cheese)
+    
+shop2 = CheeseShop('Wensleydale', cheeses)
+print('Custom iterator')
+for cheese in shop2:
+    print(cheese)
