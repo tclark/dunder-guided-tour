16be62f93eefc2d09be16ba6a6d14d39f2edfa0b
Tom Clark <tom@runawayplay.com>
2022-08-20 15:54:15 +1200

    added solutions for sequences

--- sequences/cheese_shop.py
+++ sequences/cheese_shop.py
@@ -22,4 +22,23 @@ class CheeseShop:
         # inventory is a list of Cheeses
         self.inventory = inventory
 
+    def __len__(self):
+        return len(self.inventory)
 
+    def __getitem__(self, key):
+        if isinstance(key, slice):
+            return CheeseShop(self.proprietor, self.inventory[key]
+        return self.inventory[key]
+
+   def __setitem__(self, key, val):
+       self.inventory[key] = val
+
+   del __delitem__(self, key):
+       del self.inventory[key]
+
+   def insert(self, key, val):
+       self.inventory.insert(key, val)
+
+# One big thing to take away from this example is that we are able
+# to implement our sequence easily because we are leveraging the 
+# capability of the underlying list.
